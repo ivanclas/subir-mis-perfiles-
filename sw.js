f410a5924c87f817c5b1fbfc77cca5e33bd5ab17
ivanclas <111611656+ivanclas@users.noreply.github.com>
2025-06-20 00:37:01 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
+// Función para suscribir al Push Service
 async function subscribePush() {
   const reg = await navigator.serviceWorker.ready;
   const sub = await reg.pushManager.subscribe({
     userVisibleOnly: true,
     applicationServerKey: urlBase64ToUint8Array(
-      '<TU_CLAVE_VAPID_PUBLICA>'
+      'BDfBcoJAyJJeNzareRA3yWtkY803CX4BySDR-ZLg44BbJNLXF6YAtvr4guPkLEhACN7zE7N43iAJOESIEOUWenU' // Clave pública VAPID
     )
   });
-  // Envía `sub` a tu servidor para guardar la suscripciones
+  
+  // Aquí debes enviar la suscripción al servidor para almacenarla
   console.log('Suscripción push:', JSON.stringify(sub));
 }
 
-// Helper para convertir la clave VAPID
+// Helper para convertir la clave pública VAPID
 function urlBase64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)

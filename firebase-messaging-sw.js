193dbe29b323e828c52135c4240d506f0c7c6909
ivanclas <111611656+ivanclas@users.noreply.github.com>
2025-06-21 01:41:35 -0500

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+// Importar los scripts necesarios de Firebase
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js');
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD8Qixd8Q5DFYu0a5l5jiYC2ODrUbnjwuk",
+  authDomain: "guardar-fotos-ec316.firebaseapp.com",
+  projectId: "guardar-fotos-ec316",
+  storageBucket: "guardar-fotos-ec316.appspot.com",
+  messagingSenderId: "529201655026",
+  appId: "1:529201655026:web:f10e873d825a72b5d8b46b",
+  measurementId: "G-SMEHYGBPRR"
+};
+
+// Inicializar Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// Función para recibir mensajes cuando la app está en segundo plano
+messaging.onBackgroundMessage(function(payload) {
+  console.log("Mensaje en segundo plano recibido", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon
+  };
+
+  // Mostrar la notificación en segundo plano
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});

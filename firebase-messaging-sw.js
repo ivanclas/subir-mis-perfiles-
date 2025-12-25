870c3ee361c08e7cc144d6d417b078da14d71540
ivanclas <111611656+ivanclas@users.noreply.github.com>
2025-12-25 12:06:24 -0500

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,28 +1,20 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js");
 
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyD8Qixd8Q5DFYu0a5l5jiYC2ODrUbnjwuk",
   authDomain: "guardar-fotos-ec316.firebaseapp.com",
   projectId: "guardar-fotos-ec316",
   storageBucket: "guardar-fotos-ec316.appspot.com",
   messagingSenderId: "529201655026",
-  appId: "1:529201655026:web:f10e873d825a72b5d8b46b",
-  measurementId: "G-SMEHYGBPRR"
-};
+  appId: "1:529201655026:web:f10e873d825a72b5d8b46b"
+});
 
-firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log("Mensaje en segundo plano recibido", payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
+messaging.onBackgroundMessage(payload => {
+  self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: payload.notification.icon
-  };
-
-  // Mostrar la notificación en segundo plano
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    icon: "/notification.png"
+  });
 });
